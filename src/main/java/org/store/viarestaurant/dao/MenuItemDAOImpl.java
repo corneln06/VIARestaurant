74d41833abb368b44cd19830f322a78e5ef08766
Estefano Bruno <fanobruno@gmail.com>
2026-05-10 21:55:57 +0200

    feat: not needed

--- src/main/java/org/store/viarestaurant/dao/MenuItemDAOImpl.java
+++ src/main/java/org/store/viarestaurant/dao/MenuItemDAOImpl.java
@@ -7,9 +7,7 @@ import org.store.viarestaurant.model.enums.MenuTypes;
 
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class MenuItemDAOImpl implements MenuItemDAO {
 
