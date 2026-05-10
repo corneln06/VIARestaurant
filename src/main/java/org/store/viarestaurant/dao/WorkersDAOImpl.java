9d43b5686f5709da4ac04fc885b6f36ff377727d
Negru Cornel <kornel.negru@gmail.com>
2026-05-10 22:44:02 +0200

    Changes requested by the product owner

--- src/main/java/org/store/viarestaurant/dao/WorkersDAOImpl.java
+++ src/main/java/org/store/viarestaurant/dao/WorkersDAOImpl.java
@@ -71,7 +71,7 @@ public class WorkersDAOImpl implements WorkersDAO
     ArrayList<Workers> workers = new ArrayList<>();
     try(Connection connection = getConnection()){
       PreparedStatement statement = connection.prepareStatement(
-          "SELECT id, firstName, lastName, email, rawPassword, role from workers"
+          "SELECT id, firstName, lastName, email, role from workers"
       );
       ResultSet rs = statement.executeQuery();
       while(rs.next()){
